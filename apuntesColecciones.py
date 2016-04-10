14d21320cde65ac52b16126a1e80228968651d78
L Daniel De San Pedro V <ldanieldesanpedro@gmail.com>
2016-04-10 00:55:39 -0500

    Agregué apuntes de diccionarios

--- apuntesColecciones.py
+++ apuntesColecciones.py
@@ -41,3 +41,65 @@ print(tupla4.index(3))
 #Del mismo modo podemos ver el indice de un elemento
 print(tupla4.count(2))
 #Podemos contar cuantas veces aparece un elemento especificado
+
+
+#Una forma mas elegante de crear y llenar tuplas
+
+lista = [ele for ele in (1,2,3)]
+print (lista)
+
+"""
+Las listas funcionan de manera similar que las tuplas solo que estas si son mutables,
+aparte de que estas tienen metodos para modificarlas
+
+
+"""
+
+#Metodos de adicionamiento o eliminicacion con las listas
+lista1 = [1,2,3,4,5,6,7]
+print(lista1)
+lista1.append(2)
+print(lista1)
+lista1.insert(3,12)
+print(lista1)
+del(lista1[1])
+print(lista1)
+
+#Metodos de ordenamiento de listas 
+sorted(lista1)
+print(lista1)
+sorted(lista1,reverse = True)
+print(lista1)
+#Los metodos anteriores no alteran la lista origingal, los siguientes si
+
+lista1.sort()
+print(lista1)
+lista1.reverse()
+print(lista1)
+
+#Diccionarios
+
+"""
+Un diccionario es una estructura de dato muy util , en el que por medio de una llave
+podemos acceder a un valor
+
+"""
+
+diccionario1 = dict(a=1,b=2,c=3)
+print(diccionario1)
+diccionario2 = {'a' : 8, 'b':4,"nombre":"Lalo"}
+print(diccionario2)
+
+"""
+Podemos acceder a sus contenidos con la palabra clave, ya sea para usarlos o modificarlos
+"""
+
+print(diccionario2["nombre"])
+diccionario2['a'] = 23
+print(diccionario2)
+
+
+
+
+
+
